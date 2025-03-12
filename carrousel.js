541bd0f8691b3e1c0e67f98391700e593f1e6368
Hondone01 <rmartinmantelli@gmail.com>
2025-03-12 16:25:14 -0300

    enfoque mobile first

--- carrousel.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const myCarouselElement = document.querySelector('#carousel-1');
-    const carousel = new bootstrap.Carousel(myCarouselElement, {
-        interval: 2000,  // Intervalo de 2 segundos entre las imágenes
-        ride: 'carousel', // Inicia el carrusel automáticamente
-        touch: true       // Permite el control táctil en dispositivos móviles
-    });
-});
